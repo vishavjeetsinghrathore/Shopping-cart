02d0b625fb3f4092085ce50b4f92584679a4ea23
vishavjeetsinghrathore <2021kucp1030@iiitkota.ac.in>
2023-08-26 18:43:16 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
+import { ToastContainer } from "react-toastify";
+import { Toaster } from "react-hot-toast";
+import { store } from "./redux/Store";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <Provider store={store}>
+        <App />
+        <Toaster/>  
+    </Provider>
+  </BrowserRouter>
+
+  
+);
